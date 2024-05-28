97684984b9a849e24639c4ee0b422745ee10667b
Christoph Stelz <christoph.stelz@student.kit.edu>
2024-05-28 15:00:52 +0200

    Add ReproBLAS reduction mode

---
+++ src/c_wrapper_reproblas.cpp
@@ -0,0 +1,100 @@
+#include <stdint.h>
+#include <mpi.h>
+#include <binary_tree_summation.h>
+
+extern "C" {
+#include <binned.h>
+#include <binnedBLAS.h>
+#include <reproBLAS.h>
+#include <binnedMPI.h>
+}
+
+/* TODO: Remove global state. This is a crude hack. */
+MPI_Comm default_communicator = MPI_COMM_WORLD;
+
+struct MReductionContext {
+    double *buffer;
+    size_t local_summands;
+    int rank;
+    MPI_Comm comm;
+};
+
+void set_default_reduction_context_communicator(void *communicator) {
+    MPI_Comm comm = static_cast<MPI_Comm>(communicator);
+    default_communicator = comm;
+}
+
+ReductionContext __attribute__((optimize("O0"))) new_reduction_context(int global_start_idx, int local_summands) {
+    return new_reduction_context_comm(global_start_idx, local_summands, static_cast<void *>(default_communicator));
+}
+
+ReductionContext __attribute__((optimize("O0"))) new_reduction_context_comm(int global_start_idx, int local_summands, void *communicator) {
+
+    MReductionContext *ctx = new MReductionContext();
+    ctx->buffer = new double[local_summands];
+    ctx->local_summands = local_summands;
+    ctx->comm = static_cast<MPI_Comm>(communicator);
+    MPI_Comm_rank(ctx->comm, &ctx->rank);
+
+    return ctx;
+}
+
+double * __attribute__((optimize("O0"))) get_reduction_buffer(ReductionContext ctx) {
+    auto *ptr = static_cast<MReductionContext *>(ctx);
+
+    return ptr->buffer;
+}
+
+
+union num {
+    double val;
+    unsigned char bytes[8];
+};
+
+uint64_t reduction_counter = 0;
+
+double __attribute__((optimize("O0"))) reproducible_reduce(ReductionContext ctx) {
+    auto *ptr = static_cast<MReductionContext *>(ctx);
+
+    /* Adopted from the ReproBLAS MPI_sum_sine.c example, line 105 onwards */
+    double_binned *isum = NULL;
+    double_binned *local_isum = binned_dballoc(3);
+
+    binned_dbsetzero(3, local_isum);
+
+    if (ptr->rank == 0) {
+        isum = binned_dballoc(3);
+        binned_dbsetzero(3, isum);
+    }
+
+    // Local summation
+    binnedBLAS_dbdsum(3, ptr->local_summands, ptr->buffer, 1, local_isum);
+
+    // Reducation across communicator
+    MPI_Reduce(local_isum, isum, 1, binnedMPI_DOUBLE_BINNED(3),
+            binnedMPI_DBDBADD(3), 0, ptr->comm);
+
+    double sum;
+    if (ptr->rank == 0) {
+        sum = binned_ddbconv(3, isum);
+        free(isum);
+    }
+
+    MPI_Bcast(&sum, 1, MPI_DOUBLE, 0, ptr->comm);
+
+    free(local_isum);
+    ++reduction_counter;
+    return sum;
+}
+
+void __attribute__((optimize("O0"))) free_reduction_context(ReductionContext ctx) {
+    auto *ptr = static_cast<MReductionContext *>(ctx);
+
+    delete[] ptr->buffer;
+    delete ptr;
+}
+void __attribute__((optimize("O0"))) store_summand(ReductionContext ctx, uint64_t local_idx, double val) {
+    auto *ptr = static_cast<MReductionContext *>(ctx);
+
+    ptr->buffer[local_idx] =  val;
+}
