0e2cd01960c808942314867f7f73b24ead8e558d
Christoph Stelz <christoph.stelz@student.kit.edu>
2025-02-11 20:09:04 +0000

    ReproBLAS Allreduce instead of Reduce+Bcast

--- src/reproblas_summation.cpp
+++ src/reproblas_summation.cpp
@@ -5,17 +5,12 @@ ReproblasSummation::ReproblasSummation(MPI_Comm comm, size_t local_summands)
 
   MPI_Comm_rank(comm, &rank);
   local_isum = binned_dballoc(3);
-  if (rank == 0) {
-    isum = binned_dballoc(3);
-  }
+  isum = binned_dballoc(3);
 }
 
 ReproblasSummation::~ReproblasSummation() {
   free(local_isum);
-
-  if (rank == 0) {
-    free(isum);
-  }
+  free(isum);
 }
 
 double *ReproblasSummation::getBuffer() { return buffer.data(); }
@@ -25,23 +20,17 @@ double ReproblasSummation::accumulate() {
 
   binned_dbsetzero(3, local_isum);
 
-  if (rank == 0) {
-    binned_dbsetzero(3, isum);
-  }
+binned_dbsetzero(3, isum);
 
   // Local summation
   binnedBLAS_dbdsum(3, local_summands, buffer.data(), 1, local_isum);
 
   // Reducation across communicator
-  MPI_Reduce(local_isum, isum, 1, binnedMPI_DOUBLE_BINNED(3),
-             binnedMPI_DBDBADD(3), 0, comm);
+  MPI_Allreduce(local_isum, isum, 1, binnedMPI_DOUBLE_BINNED(3),
+             binnedMPI_DBDBADD(3), comm);
 
   double sum;
-  if (rank == 0) {
-    sum = binned_ddbconv(3, isum);
-  }
-
-  MPI_Bcast(&sum, 1, MPI_DOUBLE, 0, comm);
+  sum = binned_ddbconv(3, isum);
 
   return sum;
 }
